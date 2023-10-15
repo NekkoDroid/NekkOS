343503b9622ef2c225c92e7381abd7ddadb29c9c
Michael Ferrari <nekkodroid404@gmail.com>
2023-10-15 03:41:26 +0200

    Remove development dependencies

--- setup.d/30-pacstrap.sh
+++ setup.d/30-pacstrap.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "Installing packages to the new system"
-pacstrap -K /mnt                                                                     \
-    base{,-devel} linux{,-{firmware,headers}} power-profiles-daemon bash-completion  \
-    {amd,intel}-ucode nvidia{,-{utils,prime}} broadcom-wl networkmanager fwupd       \
-    bluez{,-utils} iptables-nft pipewire{,-{audio,pulse,alsa,jack}} wireplumber      \
-    vulkan-{radeon,intel} libva-mesa-driver mesa-vdpau switcheroo-control            \
-    noto-fonts{,-{cjk,extra,emoji}} ttf-nerd-fonts-symbols{,-mono}                   \
-    gdm gnome-{shell,backgrounds,software,control-center,themes-extra}               \
-    gnome-{remote-desktop,user-share,initial-setup} nautilus                         \
-    xdg-{utils,user-dirs-gtk,desktop-portal-gnome} flatpak                           \
+pacstrap -K /mnt                                                                \
+    base linux{,-firmware} power-profiles-daemon bash-completion                \
+    {amd,intel}-ucode nvidia{,-{utils,prime}} broadcom-wl networkmanager fwupd  \
+    bluez{,-utils} iptables-nft pipewire{,-{audio,pulse,alsa,jack}} wireplumber \
+    vulkan-{radeon,intel} libva-mesa-driver mesa-vdpau switcheroo-control       \
+    noto-fonts{,-{cjk,extra,emoji}} ttf-nerd-fonts-symbols{,-mono}              \
+    gdm gnome-{shell,backgrounds,software,control-center,themes-extra}          \
+    gnome-{remote-desktop,user-share,initial-setup} nautilus                    \
+    xdg-{utils,user-dirs-gtk,desktop-portal-gnome} flatpak                      \
     neovim wl-clipboard {archlinux,pacman}-contrib pkgstats sbctl plymouth
